342d33b0e826db470b01dcceef6799fd444b7506
GiselleCastro <giselle@fisica.ufc.br>
2024-11-25 01:58:41 -0300

    type: eslint disable

--- mock/svgMock.tsx
+++ mock/svgMock.tsx
@@ -1,6 +1,6 @@
 import { forwardRef, ForwardRefExoticComponent, RefAttributes } from "react";
-// eslint-disable-next-line react/display-name
 const SvgrMock: ForwardRefExoticComponent<RefAttributes<HTMLSpanElement>> =
+  // eslint-disable-next-line react/display-name
   forwardRef((props, ref) => <span ref={ref} {...props} />);
 export const ReactComponent = SvgrMock;
 export default SvgrMock;
